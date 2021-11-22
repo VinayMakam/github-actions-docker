766df3a2f8f1389d83c18934fa7cbe8cfb27891e
Nissim Museri <55482825+nissimuseri@users.noreply.github.com>
2021-11-22 21:21:33 +0200

    edit app.py & test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,8 +5,4 @@ sys.path.append(str(Path('.').absolute().parent))
 from app.app import index, print_ip
 
 def test_index():
-    assert index() == "Hello, world!"
-
-
-def test_print_ip():
-    assert print_ip() == "172.17.0.2" or "10.1.0.10"
+    assert index() == "Hello world!"
